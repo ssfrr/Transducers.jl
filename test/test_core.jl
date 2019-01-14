865a657a3fad6386952b8cf50575e0bb76a95c7f
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-14 15:52:57 -0800

    Fix inittypeof

---
+++ test/test_core.jl
@@ -0,0 +1,34 @@
+module TestCore
+include("preamble.jl")
+
+@testset "Initializer" begin
+    @testset "Scan" begin
+        @testset for xs in iterator_variants(1:10)
+            @test_broken_if(
+                (xs isa Base.Generator),
+                collect(Scan(+, Initializer(T -> 5one(T))), xs) ==
+                    cumsum(vcat(5, collect(xs)))[2:end])
+        end
+    end
+    @testset "ScanEmit" begin
+        @testset for xs in iterator_variants(1:10)
+            @test_broken_if(
+                (xs isa Base.Generator),
+                collect(ScanEmit((u, x) -> (u + x, u + x),
+                                 Initializer(T -> 5one(T))) |> Map(first),
+                        xs) ==
+                    cumsum(vcat(5, collect(xs)))[2:end])
+        end
+    end
+    @testset "Iterated" begin
+        @testset for xs in iterator_variants(1:10)
+            @test_broken_if(
+                (xs isa Base.Generator),
+                collect(Iterated(inc, Initializer(T -> 5one(T))) |> Map(first),
+                        xs) ==
+                    (1:10) .+ (5 - 1))
+        end
+    end
+end
+
+end  # module
